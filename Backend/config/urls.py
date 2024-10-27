bad5c92f3d315a38058955e8b38546b70a0de02a
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-10-27 14:43:55 +0430

    accounts app and api apps is done

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Management Learning System Backend APIs",
+        default_version='v1',
+        description=(
+            "This is the API documentation for TESOl LMS project APIs.\n\n"
+            "Contacts:\n"
+            "- Ali Sina Sultani: alisinasultani@gmail.com\n"
+            "- Abbas Alizadah: abbas.alizadah1380@gmail.com"
+        ),
+        contact=openapi.Contact(email="alisinasultani@gmail.com"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("admin/", admin.site.urls),
+    
+] 
